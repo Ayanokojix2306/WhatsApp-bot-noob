2eab90f39da8caed47b9afe3629d82f5fb8afb59
Ayanokojix2306 <ayanokojix2306@gmail.com>
2024-10-26 00:39:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const connectDB = require('../database/mongodb.js'); // Check the path
-const connectToWhatsApp = require('./lib/koji.js');
+const connectDB = require(__dirname + '/database/mongodb'); // Updated import
+const connectToWhatsApp = require(__dirname + '/lib/koji'); // Updated import
 
 // Connect to MongoDB
 connectDB();
 
 // Start the WhatsApp bot
-connectToWhatsApp(); // Correctly invoke the function to start the bot
+connectToWhatsApp();
