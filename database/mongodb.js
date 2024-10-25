28758ba14f2a9203a4e8f0e9097dc013804ca2c9
Ayanokojix2306 <ayanokojix2306@gmail.com>
2024-10-25 20:10:48 +0100

    Update mongodb.js

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,6 +1,6 @@
 // database/mongodb.js
 const mongoose = require('mongoose');
-const { config } = require('../config');
+const config = require('../config');
 
 const connectDB = async () => {
     try {
