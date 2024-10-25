8d2e7d31b110317a7e3fdb815c76d93a4f2fd2d4
Ayanokojix2306 <ayanokojix2306@gmail.com>
2024-10-25 18:29:58 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-    MONGODB_URI: process.env.MONGODB_URI || 'your_local_mongodb_connection_string', // Fallback for local development
+    MONGODB_URI: process.env.MONGODB_URI || '', // Fallback for local development
     SERVER_PORT: process.env.PORT || 3000, // Option to set server port
     API_KEY: process.env.API_KEY || 'your_local_api_key', // Example of another config variable
 };
 
-module.exports = mongoURI;
+module.exports = config;
